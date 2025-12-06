6d1c7e4c73d65030afdeb7ad61be112ad38f8f23
Leonardo Ferreira <leoventura245@gmail.com>
2025-12-06 16:19:48 -0300

    feat: creating Report model

---
+++ feedbackSystem/src/main/java/com/techChallenge/feedbackSystem/domain/report/Report.java
@@ -0,0 +1,20 @@
+package com.techChallenge.feedbackSystem.domain.report;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Report {
+    private String description;
+    private boolean urgent;
+    private LocalDateTime sentAt;
+    private long evaluationsPerDay;
+    private long evaluationsPerUrgency;
+}
